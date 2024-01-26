e7ffe35af15df4d516871a0f82f0372fa20246f0
crystal <crystal@noreply.codeberg.org>
2024-01-26 11:35:33 -0700

    try to build higher resolution emojis

--- .ci-make.sh
+++ .ci-make.sh
@@ -4,8 +4,16 @@ case "$1" in
 	"debian-deps")
 		apt update
 		apt upgrade -y
-		apt install -y make graphicsmagick libcairo2-dev python3-nototools pngquant zopfli rsync fonttools patch which sudo
+		apt install -y make graphicsmagick libcairo2-dev python3-nototools pngquant zopfli rsync fonttools patch which sudo inkscape
 		ln -s $(which add_vs_cmap) $(which add_vs_cmap).py
+		;;
+	"twemoji-png128")
+		cd twemoji/assets
+		mkdir 128x128
+		cd svg
+		for svg in *.svg; do
+			inkscape -w 128 -h 128 $svg -o ${svg%.svg}.png
+		done
 		;;
 	"noto-patch")
 		cd noto-emoji
@@ -17,14 +25,14 @@ case "$1" in
 		cd noto-emoji
 		mv NotoColorEmoji.tmpl.ttx.tmpl Twemoji.tmpl.ttx.tmpl
 		sed -i 's/Noto Color Emoji/Twemoji/; s/NotoColorEmoji/Twemoji/; s/Copyright .* Google Inc\./Twitter, Inc and other contributors./; s/ Version .*/ '$VERSION'/; s/.*is a trademark.*//; s/Google, Inc\./Twitter, Inc and other contributors/; s,http://www.google.com/get/noto/,https://github.com/twitter/twemoji/,; s/.*is licensed under.*/      Creative Commons Attribution 4.0 International/; s,http://scripts.sil.org/OFL,http://creativecommons.org/licenses/by/4.0/,' Twemoji.tmpl.ttx.tmpl
-		cd ../twemoji/assets/72x72/
+		cd ../twemoji/assets/128x128/
 		for png in *.png; do
 			mv $png emoji_u${png//-/_}
 		done
 		;;
 	"emoji-build")
 		cd noto-emoji
-		make EMOJI=Twemoji EMOJI_SRC_DIR=../twemoji/assets/72x72 FLAGS= BODY_DIMENSIONS=76x72
+		make EMOJI=Twemoji EMOJI_SRC_DIR=../twemoji/assets/128x128 FLAGS= BODY_DIMENSIONS=136x128
 		;;
 	"zip-deps")
 		apt update
